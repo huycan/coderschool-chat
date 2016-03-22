abdaed9012ed85db78da03e6f5dfdf239643bf3e
Huy T <td.huycan@gmail.com>
2016-03-22 07:22:19 +0700

    adjust form style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def color_class_for flash_type
-    { success: 'blue-text text-darken-2', error: 'red', warning: 'alert-warning'}[flash_type.to_sym]
+    { success: 'cyan white-text', error: 'red white-text', warning: 'alert-warning'}[flash_type.to_sym]
   end
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "card white-text #{color_class_for(msg_type)}") do
+      concat(content_tag(:div, message, class: "card #{color_class_for(msg_type)}") do
         concat(content_tag(:div, message, class: "card-content white-text") do
           concat message
         end)
